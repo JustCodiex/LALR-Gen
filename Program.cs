8ed39844833c1f7eafcb7f8f2b85ed866f2b1672
JustCodiex <codiex1944@gmail.com>
2023-02-19 16:43:10 +0100

    Can now generate for a specified file.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,38 @@
 ﻿// Include parser gen
 using ParserGen;
 
+// Define target file
+string targetfile = "grammar.g";
+string prefix = string.Empty;
+if (args.Length == 1) {
+    targetfile = args[0];
+    prefix = Path.GetFileNameWithoutExtension(targetfile) + "_";
+}
+
+// Define outs
+string tableOut = prefix + "table.dat";
+string semOut = prefix + "parsersemantics.g.fs";
+string semOutBin = prefix + "parsetable.g.bin";
+string tableTxt = prefix + "table.txt";
+string tableConflict = prefix + "table.conflicts";
+
 // Log output
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine("@@ LALR-Parser generator");
-Console.WriteLine("@@ Grammar file = grammar.g");
+Console.WriteLine("@@ Grammar file = " + targetfile);
 Console.WriteLine("@@ Target Language = F#");
-Console.WriteLine("@@ Table output = table.dat");
-Console.WriteLine("@@ Language output = parsersemantics.fs");
+Console.WriteLine("@@ Table output = "+ tableOut);
+Console.WriteLine("@@ Language output = "+ semOut);
 
 // Bail if grammar is not there
-if (!File.Exists("grammar.g")) {
+if (!File.Exists(targetfile)) {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("Grammar file not implemented.");
     return;
 }
 
 // Parse grammar
-Grammar? G = Grammar.FromFile("grammar.g");
+Grammar? G = Grammar.FromFile(targetfile);
 if (G is null) {
     Console.WriteLine("Failed to read grammar file.");
     return;
@@ -28,7 +43,7 @@ G.RemoveUnusedProductions();
 
 // Generate LR table
 LR1 lr = new(G, true);
-lr.Run("table.txt", "table.conflicts");
+lr.Run(tableTxt, tableConflict);
 
 // Ensure table exists
 if (lr.Table is null) {
@@ -37,7 +52,7 @@ if (lr.Table is null) {
 }
 
 // Emit binary encoding of the table
-BinaryTableEmit.Emit(lr.Table, G, "parsetable.g.bin");
+BinaryTableEmit.Emit(lr.Table, G, semOutBin);
 
 // Emit F# code
-FSEmit.Emit(lr.Table, G, "parsersemantics.g.fs");
+FSEmit.Emit(lr.Table, G, semOut);
